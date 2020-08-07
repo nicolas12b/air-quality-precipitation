ee68eaa72906d7747195146590bccdf657c77a99
Nicolas Buitrago <nicolasbuitragobautista@gmail.com>
2020-08-07 12:50:37 -0500

    upload data to be read as df

---
+++ assingment4.py
@@ -0,0 +1,11 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as ptl 
+january_june_2017    = pd.read_excel('data/01-01-2017 to 30-06-2017.xlsx')
+july_november_2017   = pd.read_excel('data/01-07-2017 to 01-12-2017.xlsx')
+december2017_may2018 = pd.read_excel('data/02-12-2017 to 31-05-2018.xlsx')
+june_november_2018   = pd.read_excel('data/01-06-2018 to 30-11-2018.xlsx')
+december2018_may2019 = pd.read_excel('data/01-12-2018 to 31-05-2019.xlsx')   
+june_november2019    = pd.read_excel('data/01-06-2019 to 30-11-2019.xlsx')
+december2019         = pd.read_excel('data/01-12-2019 to 31-12-2019.xlsx')
